a2206a982218154c8662d8f6c76a159963c54c97
Ilia Ershov <filejunkie@gmail.com>
2024-11-17 16:19:48 +0100

    test/test event case also covered by tests

--- src/test/java/io/phasetwo/keycloak/events/WebhookSenderEventListenerProviderTest.java
+++ src/test/java/io/phasetwo/keycloak/events/WebhookSenderEventListenerProviderTest.java
@@ -1,19 +1,27 @@
 package io.phasetwo.keycloak.events;
 
 import static io.phasetwo.keycloak.Helpers.createWebhook;
+import static io.phasetwo.keycloak.Helpers.removeWebhook;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.jupiter.api.Test;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.admin.client.resource.RealmResource;
-import org.keycloak.representations.idm.RealmRepresentation;
-import org.keycloak.representations.idm.UserRepresentation;
+import org.keycloak.representations.idm.ClientRepresentation;
 
+import org.keycloak.representations.idm.RealmRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.xgp.http.server.Server;
@@ -26,6 +34,7 @@ import lombok.extern.jbosslog.JBossLog;
 
 @JBossLog
 public class WebhookSenderEventListenerProviderTest extends AbstractResourceTest {
+  final static String TEST_REALM = "testRealm";
   CloseableHttpClient httpClient = HttpClients.createDefault();
 
   String webhookUrl(String realm) {
@@ -39,7 +48,7 @@ public class WebhookSenderEventListenerProviderTest extends AbstractResourceTest
   @Test
   public void testAdminEventContainsCorrectRealmMasterMaster() throws Exception {
     // Configure
-    RealmResource realm = keycloak.realm("master");
+    RealmResource realm = keycloak.realm(REALM);
     RealmRepresentation realmRepresentation = realm.toRepresentation();
     realmRepresentation.setAdminEventsEnabled(true);
     realmRepresentation.setAdminEventsDetailsEnabled(true);
@@ -49,10 +58,10 @@ public class WebhookSenderEventListenerProviderTest extends AbstractResourceTest
     AtomicReference<String> body = new AtomicReference<String>();
     // create a server on a free port with a handler to listen for the event
     int port = WEBHOOK_SERVER_PORT;
-    createWebhook(
+    String webhookId = createWebhook(
         keycloak,
         httpClient,
-        webhookUrl("master"),
+        webhookUrl(REALM),
         "http://host.testcontainers.internal:" + port + "/webhook",
         "qlfwemke",
         ImmutableSet.of("admin.*"));
@@ -84,18 +93,21 @@ public class WebhookSenderEventListenerProviderTest extends AbstractResourceTest
 	    // check the handler for the event, after a delay
 	    String receivedPayload = body.get();
 	    ExtendedAdminEvent event = parseEvent(receivedPayload);
-	    assertThat(event.getRealmId(), equalTo("master"));
-	    assertThat(event.getAuthDetails().getRealmId(), equalTo("master"));
+	    assertThat(event.getRealmId(), equalTo(REALM));
+	    assertThat(event.getAuthDetails().getRealmId(), equalTo(REALM));
     }
     finally {
         server.stop();
+        removeWebhook(keycloak,
+                httpClient,
+                webhookUrl(REALM),
+                webhookId);
     }
   }
 
   @Test
   public void testAdminEventContainsCorrectRealmMasterTest() throws Exception {
     // Create a realm for tests
-    final String TEST_REALM = "testRealm";
     RealmRepresentation realmRepresentation = new RealmRepresentation();
     realmRepresentation.setId(TEST_REALM);
     realmRepresentation.setDisplayName(TEST_REALM);
@@ -145,7 +157,115 @@ public class WebhookSenderEventListenerProviderTest extends AbstractResourceTest
 	    String receivedPayload = body.get();
 	    ExtendedAdminEvent event = parseEvent(receivedPayload);
 	    assertThat(event.getRealmId(), equalTo(TEST_REALM));
-	    assertThat(event.getAuthDetails().getRealmId(), equalTo("master"));
+	    assertThat(event.getAuthDetails().getRealmId(), equalTo(REALM));
+
+	    // cleanup
+	    realm.remove();
+    }
+    finally {
+        server.stop();
+    }
+  }
+
+  @Test
+  public void testAdminEventContainsCorrectRealmTestTest() throws Exception {
+    // Create a realm for tests
+    RealmRepresentation realmRepresentation = new RealmRepresentation();
+    realmRepresentation.setId(TEST_REALM);
+    realmRepresentation.setDisplayName(TEST_REALM);
+    realmRepresentation.setAdminEventsEnabled(true);
+    realmRepresentation.setAdminEventsDetailsEnabled(true);
+    realmRepresentation.setRealm(TEST_REALM);
+    realmRepresentation.setEventsListeners(Arrays.asList("ext-event-webhook"));
+    realmRepresentation.setEnabled(true);
+    keycloak.realms().create(realmRepresentation);
+    RealmResource realm = keycloak.realm(TEST_REALM);
+
+    // Creating admin client
+    final String realmAdminClientId = "realmAdmin";
+    final String realmAdminClientSecret = "realmPassword";
+    ClientRepresentation client = new ClientRepresentation();
+    client.setSecret(realmAdminClientSecret);
+    client.setClientId(realmAdminClientId);
+    client.setEnabled(true);
+    client.setServiceAccountsEnabled(true);
+    client.setPublicClient(false);
+    client.setProtocol("openid-connect");
+    Response clientCreationResponse = realm.clients().create(client);
+    assertThat(clientCreationResponse.getStatus(), is(201));
+
+    // Adding rights
+    String realmManagementId = realm.clients().findByClientId("realm-management").get(0).getId();
+    String clientId = realm.clients().findByClientId(realmAdminClientId).get(0).getId();
+    String serviceUserId = realm.clients().get(clientId).getServiceAccountUser().getId();
+    List<RoleRepresentation> availableRoles = realm
+            .users()
+            .get(serviceUserId)
+            .roles()
+            .clientLevel(realmManagementId)
+            .listAvailable();
+    List<RoleRepresentation> rolesToAssign = availableRoles
+            .stream()
+            .filter(r ->
+                "realm-admin".equalsIgnoreCase(r.getName()))
+            .collect(Collectors.toList());
+    assertThat(rolesToAssign.size(), is(1));
+    realm.users().get(serviceUserId).roles().clientLevel(realmManagementId).add(rolesToAssign);
+
+    AtomicReference<String> body = new AtomicReference<String>();
+    // create a server on a free port with a handler to listen for the event
+    int port = WEBHOOK_SERVER_PORT;
+    createWebhook(
+        keycloak,
+        httpClient,
+        webhookUrl(TEST_REALM),
+        "http://host.testcontainers.internal:" + port + "/webhook",
+        "qlfwemke",
+        ImmutableSet.of("admin.*"));
+
+    Server server = new Server(port);
+    server
+        .router()
+        .POST(
+            "/webhook",
+            (request, response) -> {
+              String r = request.body();
+              log.infof("%s", r);
+              body.set(r);
+              response.body("OK");
+              response.status(202);
+            });
+    server.start();
+    Thread.sleep(1000l);
+
+    try {
+        // log in to the test realm
+        Keycloak keycloakTestRealm = KeycloakBuilder.builder()
+                .serverUrl(getAuthUrl())
+                .clientId(realmAdminClientId)
+                .clientSecret(realmAdminClientSecret)
+                .realm(TEST_REALM)
+                .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+                .build();
+        realm = keycloakTestRealm.realm(TEST_REALM);
+
+	    // cause an event to be sent
+	    UserRepresentation userRepresentation = new UserRepresentation();
+	    userRepresentation.setUsername("username");
+	    Response userResponse = realm.users().create(userRepresentation);
+	    assertThat(userResponse.getStatus(), is(201));
+
+	    Thread.sleep(1000l);
+
+	    // check the handler for the event, after a delay
+	    String receivedPayload = body.get();
+	    ExtendedAdminEvent event = parseEvent(receivedPayload);
+	    assertThat(event.getRealmId(), equalTo(TEST_REALM));
+	    assertThat(event.getAuthDetails().getRealmId(), equalTo(TEST_REALM));
+
+	    // cleanup
+	    realm = keycloak.realm(TEST_REALM);
+	    realm.remove();
     }
     finally {
         server.stop();
