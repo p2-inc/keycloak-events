234195b736a4d071bf12b988890fe45b6edc2cd1
Ilia Ershov <filejunkie@gmail.com>
2024-11-26 14:54:40 +0100

    Created from master / deleted from another realm case also covered

--- src/test/java/io/phasetwo/keycloak/events/WebhookSenderEventListenerProviderTest.java
+++ src/test/java/io/phasetwo/keycloak/events/WebhookSenderEventListenerProviderTest.java
@@ -323,6 +323,129 @@ public class WebhookSenderEventListenerProviderTest extends AbstractResourceTest
     }
   }
 
+  @Test
+  public void testAdminEventContainsCorrectRealmCreatedByMaterRemovedByTest() throws Exception
+  {
+	    // Create a realm for tests
+	    RealmRepresentation realmRepresentation = new RealmRepresentation();
+	    realmRepresentation.setId(TEST_REALM);
+	    realmRepresentation.setDisplayName(TEST_REALM);
+	    realmRepresentation.setAdminEventsEnabled(true);
+	    realmRepresentation.setAdminEventsDetailsEnabled(true);
+	    realmRepresentation.setRealm(TEST_REALM);
+	    realmRepresentation.setEventsListeners(Arrays.asList("ext-event-webhook"));
+	    realmRepresentation.setEnabled(true);
+	    keycloak.realms().create(realmRepresentation);
+	    RealmResource realm = keycloak.realm(TEST_REALM);
+
+	    // Creating admin client
+	    final String realmAdminClientId = "realmAdmin";
+	    final String realmAdminClientSecret = "realmPassword";
+	    ClientRepresentation client = new ClientRepresentation();
+	    client.setSecret(realmAdminClientSecret);
+	    client.setClientId(realmAdminClientId);
+	    client.setEnabled(true);
+	    client.setServiceAccountsEnabled(true);
+	    client.setPublicClient(false);
+	    client.setProtocol("openid-connect");
+	    Response clientCreationResponse = realm.clients().create(client);
+	    assertThat(clientCreationResponse.getStatus(), is(201));
+
+	    // Adding rights
+	    String realmManagementId = realm.clients().findByClientId("realm-management").get(0).getId();
+	    String clientId = realm.clients().findByClientId(realmAdminClientId).get(0).getId();
+	    String serviceUserId = realm.clients().get(clientId).getServiceAccountUser().getId();
+	    List<RoleRepresentation> availableRoles = realm
+	            .users()
+	            .get(serviceUserId)
+	            .roles()
+	            .clientLevel(realmManagementId)
+	            .listAvailable();
+	    List<RoleRepresentation> rolesToAssign = availableRoles
+	            .stream()
+	            .filter(r ->
+	                "realm-admin".equalsIgnoreCase(r.getName()))
+	            .collect(Collectors.toList());
+	    assertThat(rolesToAssign.size(), is(1));
+	    realm.users().get(serviceUserId).roles().clientLevel(realmManagementId).add(rolesToAssign);
+
+	    AtomicReference<String> body = new AtomicReference<String>();
+	    // create a server on a free port with a handler to listen for the event
+	    int port = WEBHOOK_SERVER_PORT;
+	    createWebhook(
+	        keycloak,
+	        httpClient,
+	        webhookUrl(TEST_REALM),
+	        "http://host.testcontainers.internal:" + port + "/webhook",
+	        "qlfwemke",
+	        ImmutableSet.of("admin.*"));
+
+	    Server server = new Server(port);
+	    server
+	        .router()
+	        .POST(
+	            "/webhook",
+	            (request, response) -> {
+	              String r = request.body();
+	              log.infof("%s", r);
+	              body.set(r);
+	              response.body("OK");
+	              response.status(202);
+	            });
+	    server.start();
+	    Thread.sleep(1000l);
+
+	    try {
+		    // cause an event to be sent
+		    UserRepresentation userRepresentation = new UserRepresentation();
+		    userRepresentation.setUsername("username");
+		    Response userResponse = realm.users().create(userRepresentation);
+		    assertThat(userResponse.getStatus(), is(201));
+
+		    Thread.sleep(1000l);
+
+		    // check the handler for the event, after a delay
+		    String receivedPayload = body.get();
+		    ExtendedAdminEvent event = parseEvent(receivedPayload);
+		    assertThat(event.getRealmId(), equalTo(TEST_REALM));
+		    assertThat(event.getAuthDetails().getRealmId(), equalTo(REALM));
+		    assertThat(event.getType(), equalTo("admin.USER-CREATE"));
+
+	        // log in to the test realm
+	        Keycloak keycloakTestRealm = KeycloakBuilder.builder()
+	                .serverUrl(getAuthUrl())
+	                .clientId(realmAdminClientId)
+	                .clientSecret(realmAdminClientSecret)
+	                .realm(TEST_REALM)
+	                .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+	                .build();
+	        realm = keycloakTestRealm.realm(TEST_REALM);
+
+		    // Now delete the user
+		    List<UserRepresentation> users = realm.users().search("username");
+		    assertThat(users.size(), is(1));
+		    String userId = users.getFirst().getId();
+		    userResponse = realm.users().delete(userId);
+		    assertThat(userResponse.getStatus(), is(204));
+
+            Thread.sleep(1000l);
+
+            // check the handler for the event, after a delay
+		    receivedPayload = body.get();
+		    event = parseEvent(receivedPayload);
+		    assertThat(event.getRealmId(), equalTo(TEST_REALM));
+		    assertThat(event.getAuthDetails().getRealmId(), equalTo(TEST_REALM));
+		    assertThat(event.getType(), equalTo("admin.USER-DELETE"));
+
+		    // cleanup
+		    realm = keycloak.realm(TEST_REALM);
+		    realm.remove();
+	    }
+	    finally {
+	        server.stop();
+	    }
+  }
+
   private static ExtendedAdminEvent parseEvent(String input) throws Exception
   {
 	  ObjectMapper mapper = new ObjectMapper();
